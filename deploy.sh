12255bc8fb01056107243113a83b676ef2ee562e
Sarabveer Singh <4297171+Sarabveer@users.noreply.github.com>
2022-03-22 10:23:59 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Composer
-cd ~/pushNotification/
 sudo wget -O composer-setup.php https://getcomposer.org/installer
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 composer require phpmailer/phpmailer
 composer update
-cd ~/
+
+git clone https://github.com/stonX-IT490/rabbitmq-common.git
+cp config.php rabbitmq-common/
