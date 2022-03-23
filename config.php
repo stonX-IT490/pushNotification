cb0552edb3da0c768fbb47a77d2b17d19d6ddfe8
Sarabveer Singh <me@sarabveer.me>
2022-03-22 20:53:32 -0400

    use pushHost rmq vhost

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ $config['host'] = "10.4.90.102";
 $config['port'] = "5672";
 $config['username'] = "dmz";
 $config['password'] = "stonx_dmz";
-$config['vhost'] = "dmzHost";
+$config['vhost'] = "pushHost";
 
 ?>
